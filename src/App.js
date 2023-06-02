af1f07b7b627e19a13d2fa74d02d037dad089ac2
Talha <talhayranci@gmail.com>
2023-06-02 03:22:04 +0100

    New Peer connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import { init } from "./services/ModelServices";
-import { joinRoom } from "trystero";
+import Peer from "peerjs";
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
-const queryParams = new URLSearchParams(window.location.search);
-const roomID = queryParams.get("roomID") ?? undefined;
+function Home() {
+	return (
+		<div className="App">
+			<Dashboard sendMessage={undefined} />
+		</div>
+	);
+}
 
-const sendMessage = roomID
-	? joinRoom({ appId: "Curio-Drive-By-Image" }, roomID).makeAction(
-			"message"
-	  )[0]
-	: undefined;
-// console.log(sendMessage, roomID, queryParams);
+function HomePeer() {
+	const { roomID } = useParams();
+	const peer = new Peer(); // Create PeerJS instance
+	const [connection, setConnection] = useState(); // Store the connection
+	const [isPeerConnected, setIsPeerConnected] = useState(false);
+
+	const peerConnection = () => {
+		if (roomID) {
+			console.log(roomID);
+
+			const conn = peer.connect(roomID);
+			setConnection(conn);
+			console.log(conn);
+			setIsPeerConnected(true);
+		}
+	};
+
+	const sendMessage = (data) => {
+		if (connection) {
+			console.log(data);
+
+			connection.send(data); // Send the message to the receiver
+		}
+	};
+
+	return (
+		<div className="App">
+			{isPeerConnected ? (
+				<Dashboard sendMessage={sendMessage} />
+			) : (
+				<Button
+					onClick={() => {
+						peerConnection();
+					}}
+					style={{
+						backgroundColor: "green",
+						marginTop: 200,
+					}}
+				>
+					CONNECT TO THE HOST DEVICE
+				</Button>
+			)}
+		</div>
+	);
+}
 
 function App() {
 	init();
 
 	return (
-		<div className="App">
-			<Dashboard sendMessage={sendMessage} />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/:roomID" element={<HomePeer />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
